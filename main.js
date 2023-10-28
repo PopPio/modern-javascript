374f51e6a5746d150dc8aed93353bc21c53f0ec0
Pop Suwichapol Jinnawong <pop_sg88@hotmail.com>
2023-10-28 10:14:17 +0700

    Episode 3 arrow function

--- main.js
+++ main.js
@@ -1,18 +1,47 @@
 'use strict';
-const user = {
-    name: 'John',
-    email: 'john@example.com'
-};
+// const user = {
+//     name: 'John',
+//     email: 'john@example.com'
+// };
 
-function init() {
-    // if (true) {
-    //     var email = 'john@example.com'
-    //     let email = 'john@example.com'
-    //     document.getElementById('output').innerHTML = email
-    // }
-    user.email = "changed@example.com"
+// function init(name, isTrue) {
+//     document.getElementById('output').innerHTML = `${user.name} ${user.email}`
+// }
 
-    document.getElementById('output').innerHTML = user.email
-}
+// const init = (name, isTrue) => {
+//     document.getElementById('output').innerHTML = `${user.name} ${user.email}`
+// }
+// init()
+// function nameString() {
+//     return `${user.name} ${user.email}`;
+// }
+// const nameString = name => `${name} ${user.email}`
+//
+// document.getElementById('output').innerHTML = nameString('Pop');
 
-init()
+let users = [
+    {
+        name: 'Andrew',
+        email: 'andrew@example.com'
+    },
+    {
+        name: 'Ashley',
+        email: 'ashley@example.com'
+    }
+]
+
+let names = [];
+
+users.forEach(user => names.push(user.name))
+
+document.getElementById('output').innerHTML = names.join(', ');
+
+document.getElementById('btn').addEventListener('click', (e) => {
+    // console.log(this)
+    const getDetails = () => {
+        console.log(e.currentTarget)
+        return `The button id is ${e.currentTarget.getAttribute('id')}`
+    }
+
+    document.getElementById('output').innerHTML = getDetails()
+});
