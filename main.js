d69433defc6fdec197e41f46705bcdb2c49f3aa0
Pop Suwichapol Jinnawong <pop_sg88@hotmail.com>
2023-10-28 11:27:02 +0700

    Episode 6 async and await

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let myPromise = new Promise((resolve, reject) => {
+/*let myPromise = new Promise((resolve, reject) => {
     // call an API
     let user = {
         name: 'John',
@@ -38,4 +38,44 @@ myPromise.then(getAdditionalUserData)
         document.getElementById('output').innerHTML = error;
     });
 
-document.getElementById('output').innerHTML = 'Loading...';
+document.getElementById('output').innerHTML = 'Loading...';*/
+
+async function init() {
+    const start = Date.now();
+    document.getElementById('output').innerHTML = `0: init()`;
+
+    const userPromise = getUserData();
+    const welcomeStringPromise = getWelcomeString();
+
+    // const user = await getUserData();
+    const user = await userPromise;
+    document.getElementById('output').innerHTML += `<br>${Date.now() - start} ${user.name} (${user.email})`;
+
+    // const welcomeString = await getWelcomeString();
+    const welcomeString = await welcomeStringPromise;
+    document.getElementById('output').innerHTML += `<br>${Date.now() - start} ${welcomeString}`;
+
+}
+
+function getUserData() {
+    return new Promise((resolve, reject) => {
+        let user = {
+            name: 'John',
+            email: 'john@example.com'
+        };
+
+        setTimeout(() => {
+            resolve(user);
+        }, 2000);
+    });
+}
+
+function getWelcomeString() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Welcome to the site!');
+        }, 2000);
+    });
+}
+
+init();
